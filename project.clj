01eccd79a2e2a0381f8d07fe588008d3330598e0
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-12-17 22:35:48 +0200

    initial

---
+++ project.clj
@@ -0,0 +1,49 @@
+(defproject rubomg "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.reader "0.8.13"]
+                 ;; CLJ
+                 [ring/ring-core "1.3.2"]
+                 [compojure "1.3.1"]
+                 [cheshire "5.4.0"]
+                 ;; CLJS
+                 [org.clojure/clojurescript "0.0-2411"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [cljs-http "0.1.22"]
+                 [secretary "1.2.1"]
+                 [om "0.8.0-beta3"]]
+
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
+            [lein-ring "0.8.13"]
+            [lein-pdo "0.1.1"]]
+
+  :aliases {"dev" ["pdo" "cljsbuild" "auto" "dev," "ring" "server-headless"]}
+
+  :ring {:handler rubomg.core/app
+         :init    rubomg.core/init}
+
+  :source-paths ["src/clj"]
+
+  :cljsbuild {
+              :builds [{:id "dev"
+                        :source-paths ["src/cljs"]
+                        :compiler {
+                                   :output-to "resources/public/js/rubomg.js"
+                                   :output-dir "resources/public/js/out"
+                                   :optimizations :none
+                                   :source-map true
+                                   :externs ["react/externs/react.js"]}}
+                       {:id "release"
+                        :source-paths ["src/cljs"]
+                        :compiler {
+                                   :output-to "resources/public/js/rubomg.js"
+                                   :source-map "resources/public/js/rubomg.js.map"
+                                   :optimizations :advanced
+                                   :pretty-print false
+                                   :output-wrapper false
+                                   :preamble ["react/react.min.js"]
+                                   :externs ["react/externs/react.js"]
+                                   :closure-warnings
+                                   {:non-standard-jsdoc :off}}}]})
---
+++ src/cljs/rubomg/core.cljs
@@ -0,0 +1,23 @@
+(ns rubomg.core
+    (:require-macros [cljs.core.async.macros :refer [go alt!]])
+    (:require [goog.events :as events]
+              [cljs.core.async :refer [put! <! >! chan timeout]]
+              [om.core :as om :include-macros true]
+              [om.dom :as dom :include-macros true]
+              [cljs-http.client :as http]
+              [rubomg.utils :refer [guid]]))
+
+;; Lets you do (prn "stuff") to the console
+(enable-console-print!)
+
+(def app-state
+  (atom {:things []}))
+
+(defn rubomg-app [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div nil
+        (dom/h1 nil "rubomg is working!")))))
+
+(om/root rubomg-app app-state {:target (.getElementById js/document "content")})
