3dec41eac2b26c93e863d9ac9a857e18d0c36404
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2014-12-19 13:02:07 +0200

    diagnostics is turned off

--- src/cljs/rubomg/yahoo/rates.cljs
+++ src/cljs/rubomg/yahoo/rates.cljs
@@ -7,7 +7,7 @@
   (let [ch (chan 1)]
     (go
      (loop []
-       (let [response (<! (http/get "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDRUB%22,%22EURRUB%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=" {:with-credentials? false}))]
+       (let [response (<! (http/get "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDRUB%22,%22EURRUB%22)&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys" {:with-credentials? false}))]
          ;pause for some time
          (let [[{usd :Rate} {eur :Rate}] (get-in response [:body :query :results :rate])]
            (>! ch {:usd (js/parseFloat usd)})
