82d5dbebffddf7f0df1dd1a623ff3cf703673522
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2015-01-05 19:03:18 +0200

    current rates are propagated to clients using web-sockets api

--- src/clj/rubomg/yahoo/rates.clj
+++ src/clj/rubomg/yahoo/rates.clj
@@ -1,8 +1,55 @@
 (ns rubomg.yahoo.rates
-  (:use org.httpkit.server))
+  (:require [clojure.core.async :refer [put! <! >! <!! chan timeout go close!]]
+            [org.httpkit.client :as client]
+            [org.httpkit.server :as server]
+            [cheshire.core :refer [parse-string generate-string]]))
+
+(defn get-url [url]
+  (let [c (chan)]
+    (client/get url {}
+                (fn [{:keys [status headers body error]}] ;; asynchronous response handling
+                  (go
+                    (if error
+                      (println "status-code" status "error" error)
+                      (do
+                        (>! c (parse-string body true))
+                        (close! c))))))
+    c))
+
+(defn usd-eur-rates []
+  (go
+    (let [{{{rates :rate} :results} :query}  (<! (get-url "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDRUB%22,%22EURRUB%22)&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"))
+          [{usd :Rate} {eur :Rate}] rates]
+        {:usd usd :eur eur})))
+
+(defn brent-rates []
+  (go
+    (let [{{{{brent :BidRealtime} :quote} :results} :query}  (<! (get-url "http://query.yahooapis.com/v1/public/yql?q=select%20BidRealtime%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22BZG15.NYM%22)&env=store://datatables.org/alltableswithkeys&format=json&diagnostics=false"))
+          ]
+        {:brent brent})))
 
 (defn ws-handler [request]
-  (with-channel request channel
-    (on-close channel (fn [status] (println "channel closed: " status)))
-    (on-receive channel (fn [data] ;; echo it back
-                          (send! channel data)))))
+  (server/with-channel request channel
+    (go
+       (loop [rates (<! (usd-eur-rates))]
+         (server/send! channel (generate-string (dissoc rates :usd)))
+         (server/send! channel (generate-string (dissoc rates :eur)))
+         (<! (timeout 5000))
+         (recur (<! (usd-eur-rates)))))
+    (go
+       (loop [brent (<! (brent-rates))]
+         (server/send! channel (generate-string brent))
+         (<! (timeout 5000))
+         (recur (<! (brent-rates)))))
+    (server/on-close channel (fn [status] (println "channel closed: " status)))
+    (server/on-receive channel (fn [data] ;; echo it back
+                          (server/send! channel data)))))
+
+(comment
+
+  (go
+   (let [res (<! (usd-eur-rates))]
+     (println "Resp: " res)
+
+   ))
+)
--- src/cljs/rubomg/yahoo/rates.cljs
+++ src/cljs/rubomg/yahoo/rates.cljs
@@ -1,27 +1,31 @@
 (ns rubomg.yahoo.rates
     (:require-macros [cljs.core.async.macros :refer [go alt!]])
-    (:require [cljs.core.async :refer [put! <! >! chan timeout]]
-              [cljs-http.client :as http]))
+    (:require [cljs.core.async :refer [put! <! >! chan timeout]]))
 
+(defn ws-socket [ws-url]
+  (new js/WebSocket ws-url))
+
+(defn ws-event[]
+  (let [ws (ws-socket "ws://localhost:8080/ws")
+        c (chan)]
+  (set! (.-onmessage ws)
+        (fn [msg]
+          (put! c
+                (js->clj
+                  (-> msg
+                      (.-data)
+                      (JSON/parse)) :keywordize-keys true)))) c))
+
+(defn map-function-on-map-vals [m f]
+        (apply merge
+               (map (fn [[k v]] {k (f v)})
+                    m)))
 (defn rates []
-  (let [ch (chan 1)]
+  (let [ch (chan 1)
+        ws-ch (ws-event)]
     (go
-     (loop []
-       (let [response (<! (http/get "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDRUB%22,%22EURRUB%22)&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys" {:with-credentials? false}))]
-         (let [[{usd :Rate} {eur :Rate}] (get-in response [:body :query :results :rate])]
-           (>! ch {:usd (js/parseFloat usd)})
-           (>! ch {:eur (js/parseFloat eur)}))
-         ;pause for some time
-         (<! (timeout 10000)))
-         (recur))
-     )
-    (go
-     (loop []
-       (let [response (<! (http/get "http://query.yahooapis.com/v1/public/yql?q=select%20BidRealtime%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22BZG15.NYM%22)&env=store://datatables.org/alltableswithkeys&format=json&diagnostics=false" {:with-credentials? false}))]
-         (let [brent (get-in response [:body :query :results :quote :BidRealtime])]
-           (>! ch {:brent (js/parseFloat brent)}))
-         ;pause for some time
-         (<! (timeout 10000)))
-         (recur))
-     )
-     ch))
+       (loop [rate (<! ws-ch)]
+         (let [rc (map-function-on-map-vals rate js/parseFloat)]
+           (>! ch rc))
+         (recur (<! ws-ch))))
+    ch))
