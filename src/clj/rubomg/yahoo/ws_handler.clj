f7d3beaac10af96ce182a2c58e0afefeb22ec016
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2015-01-08 23:50:02 +0200

    notify newly connected clients only

--- src/clj/rubomg/yahoo/ws_handler.clj
+++ src/clj/rubomg/yahoo/ws_handler.clj
@@ -7,7 +7,12 @@
 
 (let [{:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn
               connected-uids]}
-      (sente/make-channel-socket! {})]
+      (sente/make-channel-socket! {
+                                   :user-id-fn (fn [ring-request]
+                                                 (let [{:keys [params]} ring-request]
+                                                  ;fake :user-id = :client-id
+                                                   (:client-id params)))
+                                   })]
   (def ring-ajax-post                ajax-post-fn)
   (def ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn)
   (def ch-chsk                       ch-recv) ; ChannelSocket's receive channel
@@ -18,21 +23,31 @@
 (def current-rates
   (atom {:usd nil :eur nil :brent nil}))
 
+(defn has-rate-value [rate]
+  (let [currency (first (keys rate))
+          v (currency rate)]
+    (not (nil? v))))
+
 (let [cr (chan)]
   (go
-   (loop [rates (<! (usd-eur-rates))]
-     (>! cr rates)
+   (loop [{:keys [eur usd]} (<! (usd-eur-rates))]
+     (if-not (nil? eur)
+       (>! cr {:eur eur}))
+     (if-not (nil? usd)
+       (>! cr {:usd usd}))
      (<! (timeout 9000))
      (recur (<! (usd-eur-rates)))))
   (go
-   (loop [rates (<! (brent-rates))]
-     (>! cr rates)
+   (loop [rate (<! (brent-rates))]
+     (if (has-rate-value rate)
+       (>! cr rate))
      (<! (timeout 9000))
      (recur (<! (brent-rates)))))
 
   (go
    (loop [rates (<! cr)]
      (swap! current-rates (fn [r] (merge r rates)))
+     (println "rates obtained" rates)
      (chsk-send! :sente/all-users-without-uid [:omg/rate rates])
      (recur (<! cr))))
   )
@@ -44,9 +59,11 @@
           notif-clients (clojure.set/difference new-clients old-clients)
           rates @current-rates]
     ;Assign UIDs for clients and notify only newly joined clients
-;;       (doseq [uid notif-clients]
-;;         (chsk-send! uid [:omg/rate {:usd 0}]))
-    (chsk-send! :sente/all-users-without-uid [:omg/rate rates]))))
+      (doseq [uid notif-clients]
+       (print "new client notified:" uid)
+        (chsk-send! uid [:omg/rate rates]))
+    ;(chsk-send! :sente/all-users-without-uid [:omg/rate rates])
+      )))
 
 (comment
   @connected-uids
