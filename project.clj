95dc8e423d2d0abf7fc357110029bca5adfb0e08
Stanislav Perekrestov <perekrestov.stas@gmail.com>
2015-01-21 14:50:33 +0200

    deps

--- project.clj
+++ project.clj
@@ -25,7 +25,7 @@
             [lein-ring "0.9.1"]
             [lein-pdo "0.1.1"]
             [lein-figwheel "0.2.2-SNAPSHOT"]
-            [lein-ancient "0.6.0"]]
+            [lein-ancient "0.6.1"]]
 
   :aliases {"dev" ["pdo" "cljsbuild" "auto" "dev," "ring" "server-headless"]}
 
