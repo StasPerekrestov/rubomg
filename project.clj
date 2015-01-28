d4be4a016f94e9f7668af883a733fdc7abc717bb
Stanislav Perekrestov <stanislav.perekrestov@muranosoft.com>
2015-01-28 16:14:11 +0200

    deps update

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [ring/ring-defaults "0.1.3"]
                  [javax.servlet/servlet-api "2.5"]
                  ;; CLJS
-                 [org.clojure/clojurescript "0.0-2727"]
+                 [org.clojure/clojurescript "0.0-2740"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [cljs-http "0.1.24"]
                  [secretary "1.2.1"]
